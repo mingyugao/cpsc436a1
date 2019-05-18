e7aadfb24fafdd69aff57b0841ce9bde49983aaa
Mingyu Gao <mingyu.gao96@gmail.com>
2019-05-17 17:12:26 -0700

    add transition

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const pages = ['home', 'about'];
 const data = '{"messages":[{"contentText":"Hemllo"},{"contentText":"It\'s me"},{"contentText":"I was wondering if after all these years"},{"contentText":"You\'d like to meet to go over everything"}]}';
 const parsedData = JSON.parse(data);
+const newDivKeyframes = [
+  {opacity: 0.5, transform: 'translate(0, -2rem)'},
+  {opacity: 1, transform: 'translate(0, 0)'}
+];
+const oldDivsKeyframes = [
+  {transform: 'translate(0, -2rem)'},
+  {transform: 'translate(0, 0)'}
+];
 
 const hideAllPages = () => {
   pages.forEach(page => {
@@ -29,6 +37,7 @@ const addMessage = (message, pre) => {
   pre
     ? messagesContainer.prepend(newDiv)
     : messagesContainer.appendChild(newDiv);
+  return newDiv;
 };
 
 const clearMessageInput = () => {
@@ -37,11 +46,15 @@ const clearMessageInput = () => {
 
 const submitMessage = () => {
   const newMessage = document.getElementById('message-input').value;
+  const messagesContainer = document.getElementById('messages-container');
   if (newMessage) {
-    addMessage(newMessage, true);
+    for (let i = 0; i < messagesContainer.children.length; i++) {
+      messagesContainer.children[i].animate(oldDivsKeyframes, 1000);
+    }
+    const newDiv = addMessage(newMessage, true);
     const msgObj = {contentText: newMessage};
     parsedData.messages.push(msgObj);
-    console.log(parsedData.messages);
+    newDiv.animate(newDivKeyframes, 1000);
   }
   clearMessageInput();
   return false;
